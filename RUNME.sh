7c9e9137a39a5c63948043b4402e177dd9b928b8
Ashwin Nayak <anayak@mpi-magdeburg.mpg.de>
2024-05-17 23:14:57 +0200

    write output to OUT_DIR; #1

--- RUNME.sh
+++ RUNME.sh
@@ -3,30 +3,33 @@
 PROJECT_DIR="${PROJECT_DIR:=${PWD}}"
 echo -e "\e[33m[INFO] Project directory:\e[0m ${PROJECT_DIR}"
 
-BUILD_DIR="${BUILD_DIR:=build}"
+BUILD_DIR="${BUILD_DIR:=${PROJECT_DIR}/build}"
 echo -e "\e[33m[INFO] Build directory:\e[0m ${BUILD_DIR}"
+
+OUT_DIR="${OUT_DIR:=.}"
+echo -e "\e[33m[INFO] Results directory:\e[0m ${OUT_DIR}"
 
 DEMO_DIR="${BUILD_DIR}/demos"
 
 # Run single pipe testcase
 echo -e "\e[33m[DEMO] Run single_pipe_testcase\e[0m"
-pushd ${PROJECT_DIR}/demos/single_pipe
-time ${DEMO_DIR}/single_pipe/single_pipe config.json
-./plot -c config.json
+pushd ${PROJECT_DIR}/demos/single_pipe > /dev/null
+${DEMO_DIR}/single_pipe/single_pipe config.json
+./plot -c config.json -O ${OUT_DIR}
 popd > /dev/null
 
 # Run two pipe + compressor testcase
 echo -e "\e[33m[DEMO] Run two_pipe_compressor\e[0m"
-pushd ${PROJECT_DIR}/demos/two_pipe_compressor
+pushd ${PROJECT_DIR}/demos/two_pipe_compressor > /dev/null
 ${DEMO_DIR}/two_pipe_compressor/two_pipe_compressor config.json
-./plot -c config.json -p 0
-./plot -c config.json -p 1
+./plot -c config.json -p 0 -O ${OUT_DIR}
+./plot -c config.json -p 1 -O ${OUT_DIR}
 popd > /dev/null
 
 # Run four compressor types
 echo -e "\e[33m[DEMO] Run four_compressor_types\e[0m"
-pushd ${PROJECT_DIR}/demos/four_compressor_types
+pushd ${PROJECT_DIR}/demos/four_compressor_types > /dev/null
 ${DEMO_DIR}/four_compressor_types/four_compressor_types config.json
-./plot -c config.json -p 0
-./plot -c config.json -p 1
+./plot -c config.json -p 0 -O ${OUT_DIR}
+./plot -c config.json -p 1 -O ${OUT_DIR}
 popd > /dev/null
