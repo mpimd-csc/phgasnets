3fed4ef469aeac9977302431bc0b224c8f966623
Ashwin Nayak <anayak@mpi-magdeburg.mpg.de>
2024-06-22 07:25:47 +0200

    add templated State-dependent objects
    To enable autodiff, all state dependent objects need to be templated so
    that Ceres can substitute at compile-time. This has been introduced
    here and it eliminates the need for non-templated operators in
    "operators.hpp" - this will need to be removed.

---
+++ include/phgasnets/state_operators.hpp
@@ -0,0 +1,155 @@
+# pragma once
+
+# include <vector>
+# include <Eigen/Core>
+# include <Eigen/SparseCore>
+# include "operators.hpp"
+
+
+namespace PHModel{
+
+  template<typename T>
+  struct BaseStateOperator{
+
+    protected:
+      const int n_rho;
+      const int n_mom;
+
+    public:
+    std::vector<Eigen::Triplet<T>> data;
+    Eigen::SparseMatrix<T> mat;
+
+    // Constructor
+    BaseStateOperator(
+        const int n_rho,
+        const int n_mom
+    ): n_rho(n_rho), n_mom(n_mom) {}
+
+    // Update State
+    void update_state(
+      const Eigen::Ref<const Eigen::Vector<const T, Eigen::Dynamic>>&,
+      const Eigen::Ref<const Eigen::Vector<const T, Eigen::Dynamic>>&
+    ) {}
+
+    // Update entry in the operator
+    // WARN: Currently replaces only in existing non-zero entries
+    void update_ij(const int i, const int j, const T value){
+        for (int k = 0; k < data.size(); ++k)
+            if (data[k].row() == i && data[k].col() == j) {
+                data[k] = Eigen::Triplet<T>(i, j, value);
+            }
+        mat.coeffRef(i, j) = value;
+    }
+
+    // Destructor
+    virtual ~BaseStateOperator() = default;
+
+  };
+
+  template <typename T>
+  struct RStateOperator: BaseStateOperator<T>{
+
+    const double f;
+    const double D;
+
+    // Constructor
+    RStateOperator(
+        const int n_rho,
+        const int n_mom,
+        const double friction,
+        const double diameter
+    ) :
+      BaseStateOperator<T>(n_rho, n_mom), f(friction), D(diameter)
+    {
+      this->data.resize(n_mom);
+      this->mat.resize(n_rho+n_mom, n_rho+n_mom);
+    }
+
+    // (Overloaded) Update State
+    void update_state(
+        const Eigen::Ref<const Eigen::Vector<const T, Eigen::Dynamic>>& rho,
+        const Eigen::Ref<const Eigen::Vector<const T, Eigen::Dynamic>>& mom
+    ) {
+        Eigen::Vector<T, Eigen::Dynamic> friction_term =
+          (f * (mom.array()/rho.array()).abs()) / (2 * D);
+
+        for (int i = 0; i < this->n_mom; ++i)
+            this->data[i] = Eigen::Triplet<T>(
+              this->n_rho+i, this->n_rho+i, friction_term(i)
+            );
+
+        this->mat.setFromTriplets(
+          this->data.begin(), this->data.end(), [] (const T&, const T& b) { return b; }
+        );
+    }
+  }; // struct RStateOperator
+
+  template <typename T>
+  struct RtStateOperator: BaseStateOperator<T> {
+
+    private:
+      RStateOperator<T> R;
+
+    public:
+    RtStateOperator(
+      const int n_rho,
+      const int n_mom,
+      const double friction,
+      const double diameter
+    ) :
+        BaseStateOperator<T>(n_rho, n_mom),
+        RStateOperator<T>(RStateOperator<T>(n_rho, n_mom, friction, diameter))
+    {
+        this->data.resize(n_mom);
+        this->mat.resize(n_rho+n_mom+2, n_rho+n_mom+2);
+    }
+
+    void update_state(
+      const Eigen::Ref<const Eigen::Vector<const T, Eigen::Dynamic>>& rho,
+      const Eigen::Ref<const Eigen::Vector<const T, Eigen::Dynamic>>& mom
+    ) {
+      // Update the RStateOperator
+      R.update_state(rho, mom);
+      // Update self
+      this->data = R.data;
+      this->mat.setFromTriplets(
+        this->data.begin(), this->data.end(), [] (const T&, const T& b) { return b; }
+      );
+    }
+  }; // struct RtStateOperator
+
+  template <typename T>
+  struct EffortStateVec {
+    private:
+      const Y_operator Y;
+      const int n_rho;
+      const int n_mom;
+      Eigen::Vector<T, Eigen::Dynamic> vec;
+
+    public:
+      double temperature;
+      Eigen::Vector<T, Eigen::Dynamic> vec_t;
+
+      EffortStateVec(
+        const int n_rho,
+        const int n_mom,
+        const double temperature
+      ) :
+      n_rho(n_rho), n_mom(n_mom), Y(Y_operator(n_rho, n_mom)),
+      temperature(temperature),
+      vec(n_rho+n_mom), vec_t(n_rho+n_mom+2)
+      {}
+
+      void update_state(
+        const Eigen::Ref<const Eigen::Vector<const T, Eigen::Dynamic>>& rho,
+        const Eigen::Ref<const Eigen::Vector<const T, Eigen::Dynamic>>& mom
+      ){
+        vec.segment(0, n_rho) = rho * PHModel::GAS_CONSTANT * temperature;
+        vec.segment(n_rho, n_mom) = mom;
+
+        vec_t.segment(0, n_rho+n_mom) = vec;
+        vec_t.segment(n_rho+n_mom, 2) = Y.mat * vec;
+      }
+  };
+
+} // namespace PHModel
